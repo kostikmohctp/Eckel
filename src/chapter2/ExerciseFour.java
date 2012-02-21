2b05fe296a46bf95f6553290185c9ad94d8248f2
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-02-21 16:08:37 +0300

    int/int = int (!!!) remember this!

---
+++ src/chapter2/ExerciseFour.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class ExerciseFour {
+
+	static float distance = 300, time = 600;
+
+	public static void main(String[] args) {
+
+		float velocity;
+
+		System.out.println("Distance = " + distance + " m");
+		System.out.println("Time = " + time + " sec");
+
+		velocity = distance / time;
+		System.out.println("Velosity = " + velocity + " m/sec");
+
+	}
+
+}
