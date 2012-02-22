9df4c47859a27a6e1c0e2b6c78da0ee8553a8c50
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-02-22 13:55:29 +0300

    modified task 10

--- src/chapter2/ExerciseTen.java
+++ src/chapter2/ExerciseTen.java
@@ -5,24 +5,43 @@ public class ExerciseTen {
 	/**
 	 * @param args
 	 */
-	static int first = 0x1a010;
-	static int second = 0x1a011;
+	static int first = 0x1010;
+	static int second = 0x1011;
+
+	public static void logicalAnd(int firstVar, int secondVar) {
+		System.out.println("logical AND (first & second):");
+		System.out.print("decimal: " + (firstVar & secondVar) + " = binary: "
+				+ Integer.toBinaryString(firstVar & secondVar));
+		System.out
+				.println(" : if both input bits==1, then output bit=1, else bit=0");
+	}
+
+	public static void logicalOR(int firstVar, int secondVar) {
+		System.out.println("logical OR (first | second):");
+		System.out.print("decimal: " + (firstVar | secondVar) + " = binary: "
+				+ Integer.toBinaryString(firstVar | secondVar));
+		System.out
+				.println(" : if one of input bits==1, then output bit=1, else bit=0");
+	}
+
+	public static void logicalXOR(int firstVar, int secondVar) {
+		System.out.println("logical XOR (first ^ second):");
+		System.out.print("decimal: " + (firstVar ^ secondVar) + " = binary: "
+				+ Integer.toBinaryString(firstVar ^ secondVar));
+		System.out
+				.println(" : if only one of input bits==1, then output bit=1, else bit=0");
+	}
 
 	public static void main(String[] args) {
 
-		int result;
+		System.out.println("first  decimal: " + first + " = binary: "
+				+ Integer.toBinaryString(first));
+		System.out.println("second decimal: " + second + " = binary: "
+				+ Integer.toBinaryString(second));
 
-		System.out.println(first + " " + Integer.toBinaryString(first));
-		System.out.println(second + " " + Integer.toBinaryString(second));
-
-		result = first & second; // AND
-		System.out.println(result + " " + Integer.toBinaryString(result));
-
-		result = first | second; // OR
-		System.out.println(result + " " + Integer.toBinaryString(result));
-
-		result = first ^ second; // XOR
-		System.out.println(result + " " + Integer.toBinaryString(result));
+		logicalAnd(first, second);
+		logicalOR(first, second);
+		logicalXOR(first, second);
 
 	}
 
