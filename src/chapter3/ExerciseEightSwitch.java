ca32e6f55df0e457dbd7bb577d53acdcc1835e37
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-02-27 17:06:44 +0300

    small fix for switch

--- src/chapter3/ExerciseEightSwitch.java
+++ src/chapter3/ExerciseEightSwitch.java
@@ -12,6 +12,7 @@ public class ExerciseEightSwitch {
 		for (int i = 1; i < 11; i++){
 			int randomInt = rand.nextInt(10);
 			switch(randomInt){
+			case 0: System.out.println("this is a Zero: " + randomInt); break;
 			case 1: System.out.println("this is a One: " + randomInt); break;
 			case 2: System.out.println("this is a Two: " + randomInt); break;
 			case 3: System.out.println("this is a Three: " + randomInt); break;
@@ -21,7 +22,6 @@ public class ExerciseEightSwitch {
 			case 7: System.out.println("this is a Seven: " + randomInt); break;
 			case 8: System.out.println("this is a Eight: " + randomInt); break;
 			case 9: System.out.println("this is a Nine: " + randomInt); break;
-			case 10: System.out.println("this is a Ten: " + randomInt); break;
 			}
 		}
 
