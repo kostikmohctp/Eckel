f5f9e3021375d3739e62b37bdbd0d93c091327c1
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-02-29 15:40:10 +0300

    default constructor with one object

---
+++ src/chapter4initializationAndCleanup/ExerciseThreeDefaultConstructor.java
@@ -0,0 +1,21 @@
+/*Task: Create a class with a default constructor (one that takes no arguments) that prints a message. 
+ * Create an object of this class.
+ */
+
+package chapter4initializationAndCleanup;
+
+public class ExerciseThreeDefaultConstructor {
+
+	ExerciseThreeDefaultConstructor() {
+		System.out.println("ExerciseThreeDefaultConstructor...");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ExerciseThreeDefaultConstructor exThree = new ExerciseThreeDefaultConstructor();
+
+	}
+
+}
