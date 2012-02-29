ebe61ad23f79c075af07d79393dc503b5be8ddf9
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-02-29 17:33:32 +0300

    simple task

---
+++ src/chapter4initializationAndCleanup/ExerciseSeven.java
@@ -0,0 +1,19 @@
+/* Task: Create a class without a constructor, and then 
+ * create an object of that class in main( ) to verify that the default 
+ * constructor is automatically synthesized.
+ */
+
+
+package chapter4initializationAndCleanup;
+
+public class ExerciseSeven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ExerciseSeven someObject = new ExerciseSeven();
+
+	}
+
+}
