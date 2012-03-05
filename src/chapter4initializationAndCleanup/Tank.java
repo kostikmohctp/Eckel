b0c50dd74bdb3db62ad8bff644726398451db486
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-03-05 15:52:14 +0300

    tank

---
+++ src/chapter4initializationAndCleanup/Tank.java
@@ -0,0 +1,45 @@
+/* Task: Create a class called Tank that can be filled and emptied, 
+ * and has a termination condition that it must be empty when the object is cleaned up. 
+ * Write a finalize( ) that verifies this termination condition. In main( ), 
+ * test the possible scenarios that can occur when your Tank is used.
+ */
+
+package chapter4initializationAndCleanup;
+
+public class Tank {
+
+	private boolean isEmpty = false;
+
+	protected void finalize() {
+		System.out.println("Your tank has been destroyed");
+	}
+
+	void fill() {
+		isEmpty = false;
+		System.out.println("tank is full");
+	}
+
+	void empty() {
+		isEmpty = true;
+		System.out.println("tank is empty");
+	}
+
+	void checkTank() {
+		if (isEmpty) {
+			finalize();
+		}
+	}
+
+	public static void main(String[] args) {
+		Tank someTank = new Tank();
+		someTank.checkTank();
+		someTank.fill();
+		someTank.checkTank();
+		someTank.empty();
+		someTank.checkTank();
+
+		// System.gc();
+
+	}
+
+}
