08282640a2a430e411085151f046eb9d0567842d
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-03-06 18:48:09 +0300

    I dont know is it right

---
+++ src/chapter4initializationAndCleanup/Exercise15InstanceInitialization.java
@@ -0,0 +1,23 @@
+/* Task: Create a class with a String that is initialized using instance initialization.
+ */
+
+package chapter4initializationAndCleanup;
+
+public class Exercise15InstanceInitialization {
+
+	// String s001 = "s001";
+
+	Exercise15InstanceInitialization() {
+		String s001 = "s001";
+		System.out.println(s001);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Exercise15InstanceInitialization();
+
+	}
+
+}
