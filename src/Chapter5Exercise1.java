67563bfbf9edbd492e47073dc2771e2f6e2c6b3b
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-03-19 15:24:35 +0300

    Task 1 + outer class (at another package: default)

---
+++ src/Chapter5Exercise1.java
@@ -0,0 +1,13 @@
+import chapter5AccessControl.Exercise1;
+
+
+public class Chapter5Exercise1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Exercise1 ex1 = new Exercise1();
+	}
+
+}
