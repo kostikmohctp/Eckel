c2a35f45044f434ecf848f37807b49c724c9a1d3
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-04-10 14:29:15 +0300

    Task 9. A bit modifyed

--- src/anotherPackage/Exercise9Another.java
+++ src/anotherPackage/Exercise9Another.java
@@ -1,12 +1,12 @@
 package anotherPackage;
 
-import chapter5AccessControl.Exercise9;
+import chapter5AccessControl.*;
 
 public class Exercise9Another {
 
 	public static void main(String[] args) {
 		Exercise9 ex09 = new Exercise9();
-//after adding import error dissappears		
+//after adding import and get public for Exercise9 error dissappears		
 
 	}
 
