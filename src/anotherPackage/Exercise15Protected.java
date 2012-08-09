1c6b853c040e461dc5e4403e420c024042f8b4be
Konstantin_Tseruliev <Konstantin_Tseruliev@epam.com>
2012-08-09 17:01:40 +0300

    fixed 15ex

--- src/anotherPackage/Exercise15Protected.java
+++ src/anotherPackage/Exercise15Protected.java
@@ -1,13 +1,9 @@
 package anotherPackage;
 
-public class Exercise15Protected extends chapter6ReusingClasses.Ex15Protected {
-
-	/**
-	 * @param args
-	 */
+public class Exercise15Protected /* extends chapter6ReusingClasses.Ex15Protected */{
 	public static void main(String[] args) {
 		Exercise15Protected ex15Prot = new Exercise15Protected();
-		ex15Prot.protectedmethod();
+//		ex15Prot.protectedmethod();
 
 	}
 
